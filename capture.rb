e4d472a0de2f4dac0bcc2b47b4365f8f7f582cc9
Matt Steele <orphum@gmail.com>
2011-01-30 14:14:23 -0600

    capture now generates .avs files

--- capture.rb
+++ capture.rb
@@ -30,17 +30,26 @@ def get_video_defs
 end
 
 def create_avs(video_def)
+  frames = video_def[:frames]
   avs_file = ''
   avs_file << "AviSource(\"h:\\capturing\\raw\\#{video_def[:name]}.avi\")\n"
-  avs_file << 'LoadVirtualDubPlugin("H:\\Documents and Settings\\matt\\Desktop\\vdub\\plugins\\Smart.vdf", "SmartDeint", 1)\n'
-
-puts avs_file
+  avs_file << "Trim(#{video_def[:start_frame]}, #{video_def[:end_frame]}, false)\n"
+  avs_file << 'LoadVirtualDubPlugin("H:\\Documents and Settings\\matt\\Desktop\\vdub\\plugins\\Smart.vdf", "SmartDeint", 1)' << "\n"
+  avs_file << "Crop(#{frames[:left]},#{frames[:top]},-#{frames[:right]},-#{frames[:bottom]})\n"
+  avs_file << "SmartDeint(0,3,8,100,0,0,0,0,1,2,1)\n"
+  avs_file << "LanczosResize(640,480)\n"
+  avs_file << "ConvertToYV12()\n"
+  f = File.new("scripts/#{video_def[:name]}.avs", "w+")
+  f.puts avs_file
+  f.close
 end
 
 #cleanup scripts
 `rm scripts/*`
 
-def1 = get_video_defs[0]
-create_avs def1
+get_video_defs.each do |video|
+  create_avs video
+end
 
+puts "hi"
 
